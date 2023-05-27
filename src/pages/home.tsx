86b64382f5e7f373d747ec2e744e4cfc79e8986d
Kirill <lyadovkzst@gmail.com>
2023-05-28 00:10:42 +0500

    wanna sleep

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,8 @@
 import {Link} from "react-router-dom";
+import {useSearchUsersQuery} from "../store/github/github.api";
 
 export const Home = () => {
+  const {isLoading, isError, data} = useSearchUsersQuery('kirill')
   return (
     <div className='flex flex-col items-center mt-3'>
       <h1 className='text-[42px] text-center'>Home page</h1>
